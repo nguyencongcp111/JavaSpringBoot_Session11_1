0d6a12e175d236deee5527de6521fda43bfd45d3
nguyencongcp111 <nguyencongcp123@gmail.com>
2026-06-03 20:26:57 +0700

    session 11 bai 1

--- src/main/java/com/example/hospitalmanagement/service/impl/PatientsServiceImpl.java
+++ src/main/java/com/example/hospitalmanagement/service/impl/PatientsServiceImpl.java
@@ -5,9 +5,13 @@ import com.example.hospitalmanagement.models.entity.Patients;
 import com.example.hospitalmanagement.repository.PatientsRepository;
 import com.example.hospitalmanagement.service.PatientsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PatientsServiceImpl implements PatientsService {
@@ -20,6 +24,14 @@ public class PatientsServiceImpl implements PatientsService {
 
     @Override
     public Patients addPatient (PatientsDTO patientsDTO){
+        log.info("Có yêu cầu thêm bệnh nhân {}", patientsDTO.getFullName());
+
+        int minus = Period.between(patientsDTO.getDob(), LocalDate.now()).getYears();
+
+        if (minus > 120) {
+            log.warn("Số tuổi bệnh nhân quá cao {}", minus);
+        }
+
         Patients patient = Patients.builder()
                 .fullName(patientsDTO.getFullName())
                 .phoneNumber(patientsDTO.getPhoneNumber())
@@ -27,6 +39,13 @@ public class PatientsServiceImpl implements PatientsService {
                 .gender(patientsDTO.getGender())
                 .address(patientsDTO.getAddress())
                 .build();
-        return patientsRepository.save(patient);
+
+        Patients savedPatient = patientsRepository.save(patient);
+
+        log.info("Bệnh nhân {} được thêm vào với id {}",
+                savedPatient.getFullName(),
+                savedPatient.getId());
+
+        return savedPatient;
     }
 }
